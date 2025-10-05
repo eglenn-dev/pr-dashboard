6543886934d91fbfe3da99bb8f7dfb42c7ce7ce0
Ethan Glenn <ethan+git@eglenn.dev>
2025-10-05 12:15:31 -0600

    Added revalidation time to GraphQL client instantiation for caching purposes.

--- app/actions.ts
+++ app/actions.ts
@@ -151,6 +151,7 @@ export async function getAssignedPRCounts() {
         headers: {
             Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
         },
+        next: { revalidate: 300 },
     });
 
     const [allPullRequests, reviewedPullRequests] = await Promise.all([
